7f526d5db14c828d3cfb88c804c219445b2aa5f8
kant1724 <sonseungbum83@gmail.com>
2019-02-04 23:49:34 +0900

    commit

---
+++ views/static/js/main/main.js
@@ -0,0 +1,15 @@
+function ajax(url, input_data, gubun, method) {
+    $.ajax(url, {
+        type: method,
+        data: input_data,
+        async: false,
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+        dataType: 'json',
+        success: function (data, status, xhr) {},
+        error: function (jqXhr, textStatus, errorMessage) {}
+    });
+}
+
+$(document).ready(function() {
+    $(".button-collapse").sideNav();
+});
