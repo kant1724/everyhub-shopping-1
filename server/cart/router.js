79e209bb2bed10387237bad0c9d9765b1f033b38
kant1724 <sonseungbum83@gmail.com>
2019-02-04 23:11:21 +0900

    commit

--- server/cart/router.js
+++ server/cart/router.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('templates/cart', { title: 'Express' });
+    res.render('templates/cart/cart', { title: 'Express' });
 });
 
 module.exports = router;
--- server/main/router.js
+++ server/main/router.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('templates/main', { title: 'Express' });
+    res.render('templates/main/main', { title: 'Express' });
 });
 
 router.get('/whatMain', function(req, res) {
