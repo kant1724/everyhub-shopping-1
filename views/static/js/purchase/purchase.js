3f6154ba5a56b2fa2c0e9ce4057d0b9ec8d5506b
kant1724 <sonseungbum83@gmail.com>
2019-02-13 19:53:51 +0900

    commit

--- views/static/js/purchase/purchase.js
+++ views/static/js/purchase/purchase.js
@@ -12,4 +12,8 @@ function ajax(url, input_data, gubun, method) {
 
 $(document).ready(function() {
     $('.mdb-select').materialSelect();
+    $('.payment-btn').click(function() {
+       alert("주문이 완료되었습니다.\n0000-000-00000 계좌로 입금해 주세요.");
+    });
+
 });
