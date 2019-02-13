8caa7dd903bc7e94f973a0602b4c8bcaf4dd4ba3
kant1724 <sonseungbum83@gmail.com>
2019-02-13 10:45:54 +0900

    commit

--- server/cart/cartRouter.js
+++ server/cart/cartRouter.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let MobileDetect = require('mobile-detect');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('templates/cart/cart', { title: 'Express' });
+    let md = new MobileDetect(req.headers['user-agent']);
+    if (md.mobile()) {
+        res.render('templates/cart/cart-mobile', { title: 'Express' });
+    } else {
+        res.render('templates/cart/cart', { title: 'Express' });
+    }
 });
 
 module.exports = router;
