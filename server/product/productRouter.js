f25bb77b08266a318c4629ee48f2e27ba97a7566
kant1724 <sonseungbum83@gmail.com>
2019-02-16 16:50:02 +0900

    commit

--- server/product/productRouter.js
+++ server/product/productRouter.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let MobileDetect = require('mobile-detect');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('templates/product/product', { title: 'Express' });
+    let md = new MobileDetect(req.headers['user-agent']);
+    if (md.mobile()) {
+        res.render('templates/product/product-mobile', { title: 'Express' });
+    } else {
+        res.render('templates/product/product', { title: 'Express' });
+    }
 });
 
 module.exports = router;
