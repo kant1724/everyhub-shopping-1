51a20aa9466d90ffbc4140d632e154c6ab71ac7c
kant1724 <sonseungbum83@gmail.com>
2019-02-24 17:33:12 +0900

    commit

--- getter/common/auth.js
+++ getter/common/auth.js
@@ -1,38 +1,39 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
 exports.check = (req, res, next) => {
 	let token = req.query.token;
-	if (token == null) {
+	if (!token) {
 		token = req.cookies.jwt;
 	} else {
 		res.cookie('jwt', token);
 	}
-	if(!token) {
+
+	if (!token) {
 		return res.status(403).json({
 			success: false,
 			message: 'not logged in'
-		})
+		});
 	}
 
 	const p = new Promise(
 		(resolve, reject) => {
 			jwt.verify(token, req.app.get('jwt-secret'), (err, decoded) => {
-				if(err) reject(err)
+				if (err) reject(err);
 				resolve(decoded)
-			})
+			});
 		}
-	)
+	);
 
 	const respond = (token) => {
 		next();
-	}
+	};
 
 	const onError = (error) => {
 		res.status(403).json({
 			success: false,
 			message: error.message
-		})
-	}
+		});
+	};
 
-	p.then(respond).catch(onError)
-}
+	p.then(respond).catch(onError);
+};
