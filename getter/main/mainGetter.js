3c95049b69d687eacf5da814c4dc1cfd30a2e747
kant1724 <sonseungbum83@gmail.com>
2019-02-28 10:17:53 +0900

    commit

--- getter/main/mainGetter.js
+++ getter/main/mainGetter.js
@@ -2,7 +2,7 @@ let express = require('express');
 let auth = require('../common/auth').check;
 let router = express.Router();
 
-router.get('/', auth, function(req, res, next) {
+router.get('/', function(req, res, next) {
     res.render('templates/main/main', {});
 });
 
