6109a1d489db9472ed2415045336adcb7aed49c2
kant1724 <sonseungbum83@gmail.com>
2019-03-08 20:28:53 +0900

    commit

--- views/static/js/admin/order_list/order_list.js
+++ views/static/js/admin/order_list/order_list.js
@@ -15,34 +15,7 @@ function ajax(url, inputData, gubun, method) {
     });
 }
 
-let grid_data = [];
 $(document).ready(function() {
-    $("#container2").jsGrid({
-        width: "710px",
-        height: "300px",
-        filtering: false,
-        editing: false,
-        inserting: false,
-        sorting: false,
-        paging: true,
-        autoload: true,
-        pageSize: 150,
-        pageButtonCount: 10,
-        rowClick: function(args) {
-            let $row = this.rowByItem(args.item);
-            $row.children('.jsgrid-cell').css('background-color', '#B2CCFF');
-            $row.children('.jsgrid-cell').css('border-color', '#B2CCFF');
-            let orderNo = args.item['번호'];
-            $("#order_no").val(orderNo);
-        },
-        data: grid_data,
-        fields: [
-            { name: "번호", type: "hidden", width: 60, align: "center" },
-            { name: "주문자", type: "text", width: 630 },
-            { name: "텍스트", type: "text", width: 630 }
-        ]
-    });
-
     constructOrderList.init(selectOrderListMain);
 
     selectOrderListMain();
@@ -56,10 +29,5 @@ function selectOrderListMain() {
 }
 
 function selectOrderListMainCallback(ret) {
-    for (let i = 0; i < ret.length; ++i) {
-        let orderNo = ret[i].orderNo;
-        let orderDate = ret[i].orderDate;
-        let totalPrice = ret[i].totalPrice;
-    }
-
+    constructOrderList.selectCallback(ret);
 }
