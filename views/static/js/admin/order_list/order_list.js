090c19824a366e76bf2a89892280657cc7dc619c
kant1724 <sonseungbum83@gmail.com>
2019-03-09 18:54:01 +0900

    commit

--- views/static/js/admin/order_list/order_list.js
+++ views/static/js/admin/order_list/order_list.js
@@ -16,19 +16,81 @@ function ajax(url, inputData, gubun, method) {
 }
 
 $(document).ready(function() {
-    constructOrderList.init(selectOrderListMain);
-
+    $('#search').click(function() {
+        selectOrderListMain();
+    });
+    datepicker.init();
     selectOrderListMain();
 });
 
 function selectOrderListMain() {
     let inputData = {
-        lastOrderNo: constructOrderList.lastOrderNo,
-        limit: constructOrderList.idPerPage * constructOrderList.pageLength
+        startOrderDate: $('#startingDate').val(),
+        endOrderDate: $('#endingDate').val()
     };
     ajax(serverUrl + '/admin/order_list/selectOrderListMain', inputData, 'selectOrderListMain', 'POST');
 }
 
 function selectOrderListMainCallback(ret) {
-    constructOrderList.selectCallback(ret);
+    let html = '';
+    $('#order_list_tbody').empty();
+    let rowspan = {};
+    for (let i = 0; i < ret.length; ++i) {
+        let key = ret[i].orderNo;
+        if (rowspan[key] != null) {
+            rowspan[key] += 1
+        } else {
+            rowspan[key] = 1;
+        }
+    }
+    let prevOrderNo = -1;
+    for (let i = 0; i < ret.length; ++i) {
+        let orderNo = ret[i].orderNo;
+        let orderDate = ret[i].orderDate;
+        let orderPersonNm = ret[i].orderPersonNm;
+        let orderTelno = ret[i].orderTelno;
+        let itemNm1 = ret[i].itemNm1;
+        let qty = ret[i].qty;
+        if (orderNo != prevOrderNo) {
+            let rs = rowspan[orderNo];
+            html += '<tr class="each-order" style="margin-bottom: 0px;">';
+            let pt = '15px';
+            html += '<td rowspan="' + rs + '" style="vertical-align: middle; padding-top: ' + pt + ';">';
+            html += '<div id="order_no" class="order-no">' + orderNo + '</div>';
+            html += '</td>';
+            html += '<td rowspan="' + rs + '" style="vertical-align: middle; padding-top: ' + pt + ';">';
+            html += '<div id="order_date" class="oreder-date">' + orderDate + '</div>';
+            html += '</td>';
+            html += '<td rowspan="' + rs + '" style="vertical-align: middle; padding-top: ' + pt + '">';
+            html += '<div id="order_person_nm" class="order-person-nm">' + orderPersonNm + '</div>';
+            html += '</td>';
+            html += '<td rowspan="' + rs + '" style="vertical-align: middle; padding-top: ' + pt + '">';
+            html += '<div id="order_telno" class="order-telno">' + orderTelno + '</div>';
+            html += '</td>';
+            html += '<td rowspan="' + rs + '" style="vertical-align: middle; padding-top: ' + pt + '">';
+            html += '<div id="deposit_yn" class="deposit-yn"><a class="common-button-1">입금확인</a></div>';
+            html += '</td>';
+            html += '<td rowspan="' + rs + '" style="vertical-align: middle; padding-top: ' + pt + '">';
+            html += '<div id="dlvr_yn" class="dlvr-yn"><a class="common-button-1">배송완료</a></div>';
+            html += '</td>';
+            html += '<td>';
+            html += '<div id="item_nm_1" class="item-nm-1">' + itemNm1 + '</div>';
+            html += '</td>';
+            html += '<td>';
+            html += '<div id="qty" class="qty">' + qty + '</div>';
+            html += '</td>';
+            html += '</tr>';
+        } else {
+            html += '<tr>';
+            html += '<td>';
+            html += '<div id="item_nm_1" class="item-nm-1">' + itemNm1 + '</div>';
+            html += '</td>';
+            html += '<td>';
+            html += '<div id="qty" class="qty">' + qty + '</div>';
+            html += '</td>';
+            html += '</tr>';
+        }
+        prevOrderNo = orderNo;
+    }
+    $('#order_list_tbody').append(html);
 }
--- views/static/js/common/datepicker.js
+++ views/static/js/common/datepicker.js
@@ -18,3 +18,35 @@ let datepickerOption = {
     format: 'yyyy-mm-dd',
     formatSubmit: 'yyyy-mm-dd'
 };
+
+let datepicker = {
+    init: function() {
+        $('.datepicker').pickadate(datepickerOption);
+        let from_input = $('#startingDate').pickadate();
+        let from_picker = from_input.pickadate('picker');
+        let to_input = $('#endingDate').pickadate();
+        let to_picker = to_input.pickadate('picker');
+        if (from_picker.get('value')) {
+            to_picker.set('min', from_picker.get('select'))
+        }
+        if (to_picker.get('value')) {
+            from_picker.set('max', to_picker.get('select'))
+        }
+        from_picker.on('set', function (event) {
+            if (event.select) {
+                to_picker.set('min', from_picker.get('select'))
+            } else if ('clear' in event) {
+                to_picker.set('min', false)
+            }
+        });
+        to_picker.on('set', function (event) {
+            if (event.select) {
+                from_picker.set('max', to_picker.get('select'))
+            } else if ('clear' in event) {
+                from_picker.set('max', false)
+            }
+        });
+        $('#startingDate').val('2019-03-01');
+        $('#endingDate').val('2019-04-01');
+    }
+};
