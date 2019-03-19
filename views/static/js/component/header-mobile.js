5eb9202aad5ed928b9bd86bfb3359d235e5b6b1d
kant1724 <sonseungbum83@gmail.com>
2019-03-19 16:04:17 +0900

    commit

--- views/static/js/component/header-mobile.js
+++ views/static/js/component/header-mobile.js
@@ -1,46 +1,13 @@
 let headerHtml = '<nav class="top-nav navbar fixed-top navbar-expand-lg navbar-light white">';
-headerHtml += '<div class="container">';
-headerHtml += '<button id="side_nav" class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">';
-headerHtml += '<span class="navbar-toggler-icon"></span>';
-headerHtml += '</button>';
-headerHtml += '<div class="collapse navbar-collapse" id="top_menu">';
-headerHtml += '<ul class="navbar-nav">';
+headerHtml += '<div class="container pl-5 pr-5">';
 if ($('#user_no').val() == 0) {
-    headerHtml += '<li class="nav-item" style="margin-left: 0px;">';
-    headerHtml += '<a class="nav-link waves-effect waves-light dark-grey-text font-weight-bold" href="/user">';
-    headerHtml += '&nbsp;&nbsp;&nbsp;로그인';
-    headerHtml += '</a>';
-    headerHtml += '</li>';
+    headerHtml += '<div id="go_login"><div class="text-center"><i style="font-size: 22px;" class="far fa-user"></i></div><div class="text-center">로그인</div></div>';
 } else {
-    headerHtml += '<li class="nav-item" style="margin-left: 0px;">';
-    headerHtml += '<a class="nav-link waves-effect waves-light dark-grey-text font-weight-bold" href="/mypage">';
-    headerHtml += '&nbsp;&nbsp;&nbsp;마이페이지';
-    headerHtml += '</a>';
-    headerHtml += '</li>';
-    headerHtml += '<li class="nav-item" style="margin-left: 0px;">';
-    headerHtml += '<a id="logout" class="nav-link waves-effect waves-light dark-grey-text font-weight-bold">';
-    headerHtml += '&nbsp;&nbsp;&nbsp;로그아웃';
-    headerHtml += '</a>';
-    headerHtml += '</li>';
+    headerHtml += '<div id="go_mypage"><div class="text-center"><i style="font-size: 22px;" class="fas fa-user"></i></div><div class="text-center">주문내역</div></div>';
+    headerHtml += '<div id="logout"><div class="text-center"><i style="font-size: 22px;" class=" fal fa-sign-out-alt"></i></div><div class="text-center">로그아웃</div></div>';
 }
-if ($('#admin_yn').val() == 'Y') {
-    headerHtml += '<li class="nav-item" style="margin-left: 0px;">';
-    headerHtml += '<a class="admin-page nav-link waves-effect waves-light dark-grey-text font-weight-bold" href="/admin/product_manager">';
-    headerHtml += '&nbsp;&nbsp;&nbsp;상품관리';
-    headerHtml += '</a>';
-    headerHtml += '</li>';
-    headerHtml += '<li class="nav-item" style="margin-left: 0px;">';
-    headerHtml += '<a class="admin-page nav-link waves-effect waves-light dark-grey-text font-weight-bold" href="/admin/order_list">';
-    headerHtml += '&nbsp;&nbsp;&nbsp;주문목록';
-    headerHtml += '</a>';
-    headerHtml += '</li>';
-}
-headerHtml += '</ul>';
-headerHtml += '</div>';
-headerHtml += '<a id="top_center" class="logo-title navbar-brand font-weight-bold" href="/">';
-headerHtml += '<strong>간드락닷컴</strong>';
-headerHtml += '</a>';
-headerHtml += '<i id="go_shopping_cart" style="font-size: 22px;" class="pr-3 far fa-shopping-cart"></i>';
+headerHtml += '<div id="go_home"><div class="text-center"><i style="font-size: 22px;" class="fas fa-home"></i></div><div class="text-center">홈</div></div>';
+headerHtml += '<div id="go_shopping_cart"><div class="text-center"><i style="font-size: 22px;" class="far fa-shopping-cart"></i></div><div class="text-center">카트</div></div>';
 headerHtml += '</div>';
 headerHtml += '</nav>';
 
@@ -62,6 +29,22 @@ function logout(url) {
 $('#logout').click(function() {
     logout('/user/logout');
     location.href = '/user/logout';
+});
+
+$('#go_mypage').click(function() {
+    location.href = '/mypage';
+});
+
+$('#go_login').click(function() {
+    location.href = '/user';
+});
+
+$('#go_home').click(function() {
+    location.href = '/';
+});
+
+$('#go_login').click(function() {
+    location.href = '/user';
 });
 
 $('#go_shopping_cart').click(function() {
