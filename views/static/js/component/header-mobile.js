b6fe9c744d960447d98c628619e6479de20dfd3c
kant1724 <sonseungbum83@gmail.com>
2019-03-27 16:43:56 +0900

    commit

--- views/static/js/component/header-mobile.js
+++ views/static/js/component/header-mobile.js
@@ -3,7 +3,7 @@ if ($('#user_no').val() == 0) {
     headerHtml += '<div class="container">';
     headerHtml += '<div id="go_home"><div class="text-center"><i style="font-size: 16px;" class="fas fa-home"></i></div><div class="text-center" style="font-size: 11px;">홈</div></div>';
     headerHtml += '<div id="go_login"><div class="text-center"><i style="font-size: 16px;" class="far fa-sign-out-alt"></i></div><div class="text-center" style="font-size: 11px;">로그인</div></div>';
-    headerHtml += '<div id="sign_up"><div class="text-center"><i style="font-size: 16px;" class="far fa-user-plus"></i></div><div class="text-center" style="font-size: 11px;">회원가입</div></div>';
+    headerHtml += '<div id="go_sign_up"><div class="text-center"><i style="font-size: 16px;" class="far fa-user-plus"></i></div><div class="text-center" style="font-size: 11px;">회원가입</div></div>';
 } else {
     headerHtml += '<div class="container" style="padding-left: 20px; padding-right: 25px;">';
     headerHtml += '<div id="go_home"><div class="text-center"><i style="font-size: 16px;" class="fas fa-home"></i></div><div class="text-center" style="font-size: 11px;">홈</div></div>';
@@ -15,24 +15,6 @@ headerHtml += '</div>';
 headerHtml += '</nav>';
 $('header').append(headerHtml);
 
-function logout(url) {
-    $.ajax(url, {
-        type: 'POST',
-        data: '',
-        async: false,
-        xhrFields: { withCredentials: true },
-        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
-        dataType: 'json',
-        success: function (data, status, xhr) {},
-        error: function (jqXhr, textStatus, errorMessage) {}
-    });
-}
-
-$('#logout').click(function() {
-    logout('/user/logout');
-    location.href = '/user/logout';
-});
-
 $('#go_mypage').click(function() {
     location.href = '/mypage';
 });
@@ -41,7 +23,7 @@ $('#go_login').click(function() {
     location.href = '/user';
 });
 
-$('#sign_up').click(function() {
+$('#go_sign_up').click(function() {
     location.href = '/user/sign_up';
 });
 
