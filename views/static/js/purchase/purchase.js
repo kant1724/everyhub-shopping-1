7b0fbe01078e285c5c26ebb86b5a0818f4f6733b
kant1724 <sonseungbum83@gmail.com>
2019-03-30 19:31:53 +0900

    commit

--- views/static/js/purchase/purchase.js
+++ views/static/js/purchase/purchase.js
@@ -257,7 +257,7 @@ function validationCheck() {
 }
 
 function insertOrderList() {
-    orderListMain.acno = '농협: 352-0590-1078-73\n예금주: 현병윤';
+    orderListMain.acno = '\n농협: 352-0590-1078-73\n예금주: 현병윤';
     orderListMain.orderPersonNm = $('#order_person_nm').val();
     orderListMain.orderTelno = $('#order_telno_1').val() + $('#order_telno_2').val() + $('#order_telno_3').val();
     orderListMain.sendPersonNm = '';
