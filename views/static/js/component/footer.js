2ae1f6e68ca8721f46c52aa63e171d40692c8f3d
kant1724 <sonseungbum83@gmail.com>
2019-04-12 15:15:31 +0900

    commit

--- views/static/js/component/footer.js
+++ views/static/js/component/footer.js
@@ -39,7 +39,7 @@ function selectSellerInfoCallback(ret) {
 	footerHtml += '<div class="mt-2"><span id="seller_acno"></span>' + ret[0].acno + '</div>';
 	footerHtml += '<div class="mt-2"><span id="seller_deposit_person_nm">' + ret[0].depositPersonNm + '</span></div>';
 	footerHtml += '<div class="ml-2 mt-2">통신판매업: <span id="mailOrderNo">' + ret[0].mailOrderNo + '</span></div>';
-	footerHtml += '<div class="ml-2 mt-2">사업자번호: <span id="companyRegistrationNo">' + ret[0].companyRegistrationNo+ '</span></div>';
+	footerHtml += '<div class="ml-2 mt-2">사업자번호: <span id="companyRegistrationNo">' + ret[0].companyRegistrationNo + '</span></div>';
 	footerHtml += '</div>';
 	footerHtml += '</div>';
 	footerHtml += '<div class="footer-copyright py-3 text-center">';
