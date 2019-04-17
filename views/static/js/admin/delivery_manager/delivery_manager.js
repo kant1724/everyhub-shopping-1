2807dd52dc085173b7741f838a9b01818e87e467
kant1724 <sonseungbum83@gmail.com>
2019-04-17 16:35:24 +0900

    commit

--- views/static/js/admin/delivery_manager/delivery_manager.js
+++ views/static/js/admin/delivery_manager/delivery_manager.js
@@ -16,10 +16,55 @@ function ajax(url, inputData, gubun, method) {
 }
 
 $(document).ready(function() {
+    $('.new-shipping-info-btn').click(function() {
+        $('#modal_shiping_info_no').val('');
+        $('#shipping_info_modal').modal();
+    });
+
+    $('#save_shipping_info').click(function() {
+        if (isNull($('#modal_shipping_info_no').val())) {
+            insertShippingInfo();
+        } else {
+            updateShippingInfo();
+        }
+    });
+
     constructShippingInfo.init(selectShippingInfoList);
 
     selectShippingInfoList();
 });
+
+function insertShippingInfo() {
+    let inputData = {
+        addr1: $('#modal_addr_1').val(),
+        addr2: $('#modal_addr_2').val(),
+        addr3: $('#modal_addr_3').val(),
+        zipNo: $('#modal_zip_no').val(),
+        shippingFee: $('#modal_shipping_fee').val()
+    };
+
+    ajax(serverUrl + '/admin/delivery_manager/insertShippingInfo', inputData, 'insertShippingInfo', 'POST');
+}
+
+function updateShippingInfo() {
+    let inputData = {
+        shippingInfoNo: $('#modal_shipping_info_no').val(),
+        addr1: $('#modal_addr_1').val(),
+        addr2: $('#modal_addr_2').val(),
+        addr3: $('#modal_addr_3').val(),
+        zipNo: $('#modal_zip_no').val(),
+        shippingFee: $('#modal_shipping_fee').val()
+    };
+
+    ajax(serverUrl + '/admin/delivery_manager/updateShippingInfo', inputData, 'updateShippingInfo', 'POST');
+}
+
+function deleteShippingInfo() {
+    let inputData = {
+    };
+
+    ajax(serverUrl + '/admin/delivery_manager/deleteShippingInfo', inputData, 'deleteShippingInfo', 'POST');
+}
 
 function selectShippingInfoList() {
     let inputData = {
