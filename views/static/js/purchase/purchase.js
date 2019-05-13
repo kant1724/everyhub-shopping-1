0ed9ad9c7271369d90197c802107d93dce54aa1d
kant1724 <sonseungbum83@gmail.com>
2019-05-13 14:46:20 +0900

    commit

--- views/static/js/purchase/purchase.js
+++ views/static/js/purchase/purchase.js
@@ -17,6 +17,8 @@ function ajax(url, inputData, gubun, method) {
                 selectOneItemCallback(data.ret);
             } else if (gubun == 'selectShippingInfoByZipNo') {
                 selectShippingInfoByZipNoCallback(data.ret);
+            } else if (gubun == 'selectRecentReceiver') {
+                selectRecentReceiverCallback(data.ret);
             }
         },
         error: function (jqXhr, textStatus, errorMessage) {}
@@ -25,8 +27,48 @@ function ajax(url, inputData, gubun, method) {
 
 let orderListDetail = [];
 let orderListMain = {};
+let recentReceiver = [];
+let rowNo;
 $(document).ready(function() {
     $('.mdb-select').materialSelect();
+
+    $("#container").jsGrid({
+        width: "100%",
+        height: "300px",
+        filtering: false,
+        editing: false,
+        inserting: false,
+        sorting: false,
+        paging: true,
+        autoload: true,
+        pageSize: 150,
+        pageButtonCount: 10,
+        data: recentReceiver,
+        fields: [
+            { name: "받는자 전화번호", type: "text", width: 100 },
+            { name: "받는자 주소", type: "text", width: 150, align: "center" }
+        ],
+        rowClick: function(args) {
+            let $row = this.rowByItem(args.item);
+            $row.children('.jsgrid-cell').css('background-color', '#B2CCFF');
+            $row.children('.jsgrid-cell').css('border-color', '#B2CCFF');
+            rowNo = args.item['번호'];
+            for (let i = 0; i < recentReceiverData.length; ++i) {
+                if (recentReceiverData[i].num == rowNo) {
+                    $('#receive_person_nm').val(recentReceiverData[i].receivePersonNm);
+                    $('#receive_person_nm').focus();
+                    $('#receive_telno_1').val(recentReceiverData[i].receiveTelno1);
+                    $('#receive_telno_2').val(recentReceiverData[i].receiveTelno2);
+                    $('#receive_telno_3').val(recentReceiverData[i].receiveTelno3);
+                    $('#receive_zip_no').text(recentReceiverData[i].receiveZipNo);
+                    $('#receive_address_main').text(recentReceiverData[i].receiveAddressMain);
+                    $('#receive_address_detail').val(recentReceiverData[i].receiveAddressDetail);
+                    $('#receive_address_detail').focus();
+                    $('#recent_receiver_close_modal').click();
+                }
+            }
+        }
+    });
 
     $('.payment-btn').click(function() {
         if (validationCheck()) {
@@ -94,11 +136,26 @@ $(document).ready(function() {
         searchAddressApi.init();
     });
 
+    $('#search_send_address').click(function() {
+        $('#zip_no_id').val('send_zip_no');
+        $('#address_main_id').val('send_address_main');
+        $('#address_modal').modal();
+        searchAddressApi.init();
+    });
+
     $('#search_receive_address').click(function() {
         $('#zip_no_id').val('receive_zip_no');
         $('#address_main_id').val('receive_address_main');
         $('#address_modal').modal();
         searchAddressApi.init();
+    });
+
+    $('#recent_receiver').click(function() {
+        $('#recent_receiver_modal').modal();
+    });
+
+    $('#search_recent_receiver_modal').click(function() {
+        selectRecentReceiver();
     });
 
     $('#order_telno_1').keyup(function(event) {
@@ -436,4 +493,38 @@ function selectShippingInfoByZipNoCallback(ret) {
         $('#additional_shipping_fee_text').show();
         $('#total_price_text').text('총 결제금액: ' + numberWithCommas(orderListMain.totalPrice + orderListMain.additionalShippingFee) + '원');
     }
+}
+
+function selectRecentReceiver() {
+    let inputData = {
+        telno: $('#telno_subject').val()
+    };
+    ajax(serverUrl + '/admin/order_list/selectRecentReceiver', inputData, 'selectRecentReceiver', 'POST');
+}
+
+let recentReceiverData = [];
+function selectRecentReceiverCallback(ret) {
+    recentReceiver = [];
+    recentReceiverData = [];
+    for (let i = 0; i < ret.length; ++i) {
+        let a = {};
+        a['번호'] = i;
+        a['받는자 전화번호'] = ret[i].receiveTelno;
+        a['받는자 주소'] = ret[i].receiveZipNo + ' ' + ret[i].receiveAddressMain + ' ' + ret[i].receiveAddressDetail;
+        recentReceiver.push(a);
+
+        let b = {};
+        b.num = i;
+        b.receivePersonNm = ret[i].receivePersonNm;
+        b.receiveTelno1 = ret[i].receiveTelno1;
+        b.receiveTelno2 = ret[i].receiveTelno2;
+        b.receiveTelno3 = ret[i].receiveTelno3;
+        b.receiveZipNo = ret[i].receiveZipNo;
+        b.receiveAddressMain = ret[i].receiveAddressMain;
+        b.receiveAddressDetail = ret[i].receiveAddressDetail;
+
+        recentReceiverData.push(b);
+    }
+
+    $("#container").jsGrid("option", "data", recentReceiver);
 }
