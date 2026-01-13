eeb38f8eb03bf0aaf6228594061804e821b2b0ed
User1 <sonseungbum@naver.com>
2026-01-13 18:41:09 +0900

    commit

--- views/static/js/purchase/purchase.js
+++ views/static/js/purchase/purchase.js
@@ -453,9 +453,9 @@ function insertOrderList() {
     orderListMain.sendTelno1 = '';
     orderListMain.sendTelno2 = '';
     orderListMain.sendTelno3 = '';
-    orderListMain.sendZipNo = '63245';
-    orderListMain.sendAddressMain = '제주특별자치도 제주시 간월동로5길 43';
-    orderListMain.sendAddressDetail = '';
+    orderListMain.sendZipNo = '63246';
+    orderListMain.sendAddressMain = '제주특별자치도 제주시 간월동로 54';
+    orderListMain.sendAddressDetail = '제주품은간드락';
     if (!$('#send_person_nm').val()) {
         orderListMain.sendPersonNm = '현병윤';
     } else {
